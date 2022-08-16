3f2396af0054f0794d0f51088534d0392c573130
Carlos Vinicius Soares da Silva <carlos.silva@redeatelco.local>
2022-08-16 11:25:29 -0300

    salvando

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express"
-import { v4 as uuidV4 } from "uuid"
 import { Category } from "../models/Category";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository()
@@ -10,13 +10,8 @@ const categoriesRepository = new CategoriesRepository()
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const categoryAlreadyExist = categoriesRepository.findByName(name);
-
-    if(categoryAlreadyExist){
-        return response.status(400).json({ error: "Category Already exists!"})
-    }
-
-    categoriesRepository.create({name, description})
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+    createCategoryService.execute({ name, description })
 
     return response.status(201).send();
 })
